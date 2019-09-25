fadc4b37312c8f39f93b401fb6d4de954d1da15a
Hokedm <hokedm@dukes.jmu.edu>
2019-09-25 12:14:59 -0400

    updating vue file to lesson 2 part 6

--- vuemast/real-world-vue/src/views/EventList.vue
+++ vuemast/real-world-vue/src/views/EventList.vue
@@ -1,9 +1,17 @@
 <template>
   <dir>
     <h1>Events Listing</h1>
-
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link>
+    <EventCard />
   </dir>
 </template>
 
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
 
